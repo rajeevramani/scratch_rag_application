8a571fcfa3bcdb2675c9b677f6b5e9f4f63058a7
rajeev ramani <rajeev.ramani@gmail.com>
2024-11-18 15:28:52 +1100

    adding the rest of the files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Generic RAG Application
+
+## Build plan
+
+- [ * ] Setup functionality to read from a config file.
+- [ * ] Set up simple logging for debugging purpose.
+- [ * ] Set up simple functionality to read website information
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import asyncio
 from scratch_rag_application.utils.logging_config import setup_logging
 import logging
 from scratch_rag_application.loader.url_loader import URLLoader
+from scratch_rag_application.text_splitter.splitter_factory import TextSplitterFactory
+from scratch_rag_application.config.config_handler import ConfigHandler
 
 setup_logging()
 
@@ -11,8 +13,13 @@ async def main():
     logger = logging.getLogger(__name__)
     url_loader = URLLoader()
     docs = await url_loader.load_urls()
-    logger.info(f"docs: {docs[:1]}")
-
+    logger.info(f"docs retrieved: {len(docs)}")
+    config = ConfigHandler("config.yaml")
+    t_factory = TextSplitterFactory(config)
+    b_splites = t_factory.create_splitter()
+    s_docs = b_splites.split_documents(docs)
+    print(f"docs split: {len(s_docs)}")
+    print(f"docs split: {s_docs[:3]}")
 
 if __name__ == "__main__":
     asyncio.run(main())
